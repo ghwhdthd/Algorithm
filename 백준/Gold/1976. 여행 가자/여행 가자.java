0d08b6c6f52b9de40ae012a5dcb78a0b8980012d
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-02-13 14:02:18 +0900

    [Gold IV] Title: 여행 가자, Time: 188 ms, Memory: 17536 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -4,51 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int N,M;
+    private static int[] parent;
 
-    static int N,M;
-    static int[][] inputArr;
-    static int[] plan;
-    static int[] city;
-    static int[] parent;
-    static boolean check =true;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); //~200
-        M = Integer.parseInt(br.readLine()); //~1000
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        parent = new int[N+1];
+        int[] plan = new int[M];
+        //init
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
 
-        inputArr = new int[N+1][N+1]; // 1부터
-        city = new int [N+1];
-        parent = new int [N+1];
-        plan = new int[M];
 
-        init();
-
-        StringTokenizer st = null;
-        for(int i=1; i<=N; i++){
+        StringTokenizer st;
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=N; j++){
-                inputArr[i][j] = Integer.parseInt(st.nextToken());
-                if(inputArr[i][j] == 1){
+            for (int j = 1; j <=N ; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if(n == 1) {
                     union(i,j);
                 }
-
             }
         }
-        int superParent = -1;
+
         st = new StringTokenizer(br.readLine());
-        for(int i =0; i<M; i++){
+
+        for (int i = 0; i < M; i++) {
             plan[i] = Integer.parseInt(st.nextToken());
-            if(i == 0){
-                superParent = find(plan[i]);
-            }
-            else{
-                if(find(plan[i])!= superParent){
-                    check = false;
-                    break;
-                }
-            }
         }
 
+
+        int tmpParent = find(plan[0]);
+        boolean check = true;
+        for (int i = 0; i < M; i++) {
+            if(tmpParent != find(plan[i])){
+                check = false;
+                break;
+            }
+        }
         if(check){
             System.out.println("YES");
         }else{
@@ -56,24 +52,17 @@ public class Main {
         }
 
     }
-    private static void init(){
-        for(int i =1; i<=N; i++){
-            city[i] = i;
-            parent[i] = i;
-        }
-    }
-    private static int find(int x){
-        if(parent[x] == x) return x;
-        //부모도 최신화
-        return parent[x] = find(parent[x]);
-    }
-    private static void union(int a, int b){
+
+    private static void union(int a,int b){
         int aRoot = find(a);
         int bRoot = find(b);
-        if(aRoot == bRoot){
-            return;
-        }
+        if(aRoot == bRoot) return;
+
         parent[bRoot] = aRoot;
     }
+    private static int find(int a){
+        if(a == parent[a]) return a;
 
+        return parent[a]=find(parent[a]);
+    }
 }
