b3f1824de39d5ca61af668120e28f085407fd7f4
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-03-12 18:45:17 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 그룹별 조건에 맞는 식당 목록 출력하기 - 131124 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131124) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131124#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 12일 18:34:11
+2024년 03월 12일 18:45:16
 
 ### 문제 설명
 
--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,15 +1,15 @@
-WITH sub AS (
-    SELECT MEMBER_ID, COUNT(MEMBER_ID)
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY COUNT(MEMBER_ID) desc
-    LIMIT 1
+WITH sub AS(
+    select MEMBER_ID, count(MEMBER_ID)
+    from REST_REVIEW
+    group by MEMBER_ID
+    order by count(MEMBER_ID) DESC
+    limit 1
 )
 
-SELECT MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE as M 
-left join REST_REVIEW as R
-on M.MEMBER_ID = R.MEMBER_ID
-WHERE M.MEMBER_ID = (SELECT MEMBER_ID
-                   FROM sub)
-ORDER BY REVIEW_DATE, REVIEW_TEXT;
+select M.MEMBER_NAME, R.REVIEW_TEXT, date_format(REVIEW_DATE,'%Y-%m-%d') as REVIEW_DATE
+    from MEMBER_PROFILE as M
+    left join REST_REVIEW as R
+    on M.MEMBER_ID = R.MEMBER_ID
+    where M.MEMBER_ID = (select MEMBER_ID from sub)
+    order by 3,2
+;
