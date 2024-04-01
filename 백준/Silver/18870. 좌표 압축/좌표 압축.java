77d24ab7790a757d25805f748569c504c203ff03
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-04-02 02:28:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1908 ms, Memory: 265860 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,76 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     private static int N;
-    private static int[] point;
-    private static int[] sortPoint;
-    private static boolean[] visit;
-    private static int[] res;
+    private static int[] input;
+    private static int[] sortInput;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        Set<Integer> set = new HashSet<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        point = new int[N];
-        sortPoint = new int[N];
-        visit = new boolean[N];
-        res = new int[N];
-        //값을 key로 하고 value를 index 리스트로 해서
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
+        N = Integer.parseInt(br.readLine());
+        input = new int[N];
+        sortInput = new int[N];
+        Map<Integer,Integer> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            point[i] = Integer.parseInt(st.nextToken());
-            sortPoint[i] = point[i];
+            input[i] = Integer.parseInt(st.nextToken());
+            sortInput[i] = input[i];
+        }
 
-            if(map.get(point[i]) == null){
-                List<Integer> indexLi = new ArrayList<>();
-                indexLi.add(i);
-                map.put(point[i], indexLi);
-            }else{
-                List<Integer> indexLi = map.get(point[i]);
-                indexLi.add(i);
-                map.put(point[i],indexLi);
+        Arrays.sort(sortInput);
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if(!map.containsKey(sortInput[i])){
+                map.put(sortInput[i], cnt++);
             }
         }
-
         for (int i = 0; i < N; i++) {
-            int before = set.size();
-            set.add(point[i]);
-            int after = set.size();
-            if(before != after) pq.add(point[i]);
-        }
-
-        while (!pq.isEmpty()){
-            int key = pq.poll();
-            int size = pq.size();
-            List<Integer> indexLi = map.get(key);
-            for(int index : indexLi){
-                if(visit[index]) continue;
-                res[index] = size;
-                visit[index] = true;
-
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            sb.append(res[i]).append(" ");
+            sb.append(map.get(input[i])).append(" ");
         }
         System.out.println(sb);
 
-
     }
-
 }
