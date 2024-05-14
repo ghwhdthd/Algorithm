d2b0f6d8b9db52b437d2b6ac8ce2db16e8fa9a7f
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-05-14 17:16:48 +0900

    [Silver IV] Title: 수 찾기, Time: 1336 ms, Memory: 58568 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer,Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map.put(Integer.parseInt(st.nextToken()), 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            if(map.get(now) == null){
+                System.out.println(0);
+            }else{
+                System.out.println(1);
+            }
+        }
+
+    }
+
+}
