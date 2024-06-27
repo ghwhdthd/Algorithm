01355bf66f84e1859e032ad69d7328dc71b5b628
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-06-27 19:10:16 +0900

    [Gold IV] Title: 월드컵, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -4,94 +4,85 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    //조합으로 2팀을 뽑고
+    // 2팀의 대전은 승 무 패 중 하나임 -> 이를 dfs로 확인 -> 끝까지 다 확인된 시점에 바로 return
+
+    private static int[] candi;
     private static int[][] matches;
     private static int[][] worldCup;
-    private static boolean isOk;
+    private static boolean isEnd;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 6C2 = 15 이므로 총 15경기를 진행
-        // 각 경기에 참여하는 팀 숫자 저장
-            // 1경기에 1번이랑 2번이 매치했으면 -> matches[1][0] = 1, matches[1][1] = 2
+        StringBuilder sb = new StringBuilder();
+        candi = new int[6];
         matches = new int[15][2];
-        fillMatches();
-
-        //input
+        for (int i = 0; i < 6; i++) {
+            candi[i] = i;
+        }
         worldCup = new int[6][3];
-        
-        StringTokenizer st;
 
         for (int i = 0; i < 4; i++) {
-            isOk = false;
-            st = new StringTokenizer(br.readLine());
-            if (!fillWorldCup(st)) {
-                System.out.print(0 + " ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            isEnd = false;
+            if(!fill(st)) {
+                sb.append(0 + " ");
                 continue;
             }
+            setMatches();
 
             dfs(0);
-            
-            
-            if(isOk){
-                System.out.print(1 + " ");
+            if(isEnd){
+                sb.append(1 + " ");
             }else{
-                System.out.print(0 + " ");
+                sb.append(0 + " ");
             }
-
-
         }
+        System.out.println(sb);
 
     }
-    private static void dfs(int matchCount){
-        if(isOk) return;
-        if(matchCount >= 15) {
-            isOk = true;
+
+    private static void dfs(int matchNum){
+        if(isEnd) return;
+        if(matchNum == 15){
+            isEnd = true;
             return;
         }
 
+        int aTeam = matches[matchNum][0];
+        int bTeam = matches[matchNum][1];
 
-        int aTeam = matches[matchCount][0];
-        int bTeam = matches[matchCount][1];
-
-        //aTeam 승 bTeam 패
+        //승
         if(worldCup[aTeam][0] > 0 && worldCup[bTeam][2] > 0){
             worldCup[aTeam][0]--;
             worldCup[bTeam][2]--;
-            dfs(matchCount+1);
-
-            //원상복귀
+            dfs(matchNum + 1);
             worldCup[aTeam][0]++;
             worldCup[bTeam][2]++;
 
         }
-
-
-        //aTeam 패 bTeam 승
+        //패
         if(worldCup[aTeam][2] > 0 && worldCup[bTeam][0] > 0){
             worldCup[aTeam][2]--;
             worldCup[bTeam][0]--;
-            dfs(matchCount+1);
-
+            dfs(matchNum + 1);
             worldCup[aTeam][2]++;
             worldCup[bTeam][0]++;
 
         }
-        // 무승부
+        //무
         if(worldCup[aTeam][1] > 0 && worldCup[bTeam][1] > 0){
             worldCup[aTeam][1]--;
             worldCup[bTeam][1]--;
-            dfs(matchCount+1);
-
+            dfs(matchNum + 1);
             worldCup[aTeam][1]++;
             worldCup[bTeam][1]++;
         }
 
-
-
     }
-
-    private static void fillMatches(){
+    private static void setMatches(){
         int matchCount = 0;
-
         for (int i = 0; i < 6; i++) {
             for (int j = i + 1; j < 6; j++) {
                 matches[matchCount][0] = i;
@@ -101,17 +92,17 @@ public class Main {
         }
     }
 
-    private static boolean fillWorldCup(StringTokenizer st ){
-        for(int i = 0; i < 6; i++){
-            int win = Integer.parseInt(st.nextToken());
-            int draw = Integer.parseInt(st.nextToken());
-            int lose = Integer.parseInt(st.nextToken());
-            worldCup[i][0] = win;
-            worldCup[i][1] = draw;
-            worldCup[i][2] = lose;
 
-            // 모든 팀은 5번의 경기를 치뤘어야함
-            if(win + draw + lose != 5) return false;
+    private static boolean fill(StringTokenizer st ){
+        for (int i = 0; i < 6; i++) {
+            int w = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+
+            if(w + d + l != 5) return false;
+            worldCup[i][0] = w;
+            worldCup[i][1] = d;
+            worldCup[i][2] = l;
         }
         return true;
     }
