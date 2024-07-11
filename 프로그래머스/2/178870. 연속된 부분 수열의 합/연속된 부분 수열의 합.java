6431b84b8859012bba6a273bf482436bb461cdcf
song__ho <76081770+ghwhdthd@users.noreply.github.com>
2024-07-11 15:48:48 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.60 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    // 투포인터로 풀면 됨
+    private static PriorityQueue<Node> pq;
+    
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+        pq = new PriorityQueue();
+        int r = 0;
+        int l = 0;
+        int now = sequence[0];
+        while(true){
+            if(r >= sequence.length) break;
+            if(l > r) break;
+            
+            // System.out.println(l + " " + r +  " " + now);
+            if (now <= k){
+                if(now == k) pq.add(new Node(r, l, r-l));
+                r++;
+                if(r >= sequence.length) break;
+                now += sequence[r];
+                
+                
+            }else{
+                now -= sequence[l];
+                l++;
+            }
+            
+        }
+        
+        
+        Node res = pq.poll();
+        
+        answer[0] = res.l;
+        answer[1] = res.r; 
+        return answer;
+    }
+    private static class Node implements Comparable<Node>{
+        int r;
+        int l;
+        int dif;
+        public Node(int r, int l, int dif){
+            this.r = r;
+            this.l = l;
+            this.dif = dif;
+        }
+        
+        public int compareTo(Node o){
+            if(this.dif == o.dif) return this.l - o.l;
+            return this.dif - o.dif;
+            
+        }
+    }
+}
